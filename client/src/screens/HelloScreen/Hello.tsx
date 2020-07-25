d8e5ec51aa1477103fa5d47efa339f50378c6ca9
navidbarsalari <barsalarinavid@yahoo.com>
2020-07-25 09:53:04 +0430

    add test

--- client/src/screens/HelloScreen/Hello.tsx
+++ client/src/screens/HelloScreen/Hello.tsx
@@ -8,52 +8,23 @@ export interface Props {
   onDecrement?: () => void;
 }
 
-export class Hello extends React.Component {
-  constructor(props) {
-    super(props);
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error("You could be a little more enthusiastic. :D");
   }
 
-  componentDidCatch() {}
-  componentDidUpdate() {}
-  componentDidMount() {}
-  componentWillMount() {}
-  componentWillReceiveProps() {}
-  componentWillUnmount() {}
-  componentWillUpdate() {}
-
-  render() {
-    const { name, enthusiasmLevel = 1, onIncrement, onDecrement } = this.props;
-    return (
-      <div className="hello">
-        <div className="greeting">
-          Hello {name + getExclamationMarks(enthusiasmLevel)}
-        </div>
-        <div>
-          <button onClick={onDecrement}>-</button>
-          <button onClick={onIncrement}>+</button>
-        </div>
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
       </div>
-    );
-  }
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
+      </div>
+    </div>
+  );
 }
-
-// function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
-//   if (enthusiasmLevel <= 0) {
-//     throw new Error("You could be a little more enthusiastic. :D");
-//   }
-
-//   return (
-//     <div className="hello">
-//       <div className="greeting">
-//         Hello {name + getExclamationMarks(enthusiasmLevel)}
-//       </div>
-//       <div>
-//         <button onClick={onDecrement}>-</button>
-//         <button onClick={onIncrement}>+</button>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default Hello;
 
