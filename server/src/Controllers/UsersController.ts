653ff3865bd434ef12b093790a10521bf9b24943
navidbarsalari <barsalarinavid@yahoo.com>
2020-07-25 09:54:32 +0430

    inital commit

--- server/src/Controllers/UsersController.ts
+++ server/src/Controllers/UsersController.ts
@@ -7,11 +7,12 @@ export class UsersController {
   constructor() {
     this.router = new Router();
     this.init();
+    // this is a test
   }
 
   private init(): void {
     this.router.get("/", this.getAll);
-    this.router.get("/id:id", this.getById)
+    this.router.get("/id:id", this.getById);
   }
 
   public getAll(req: Request, res: Response, next: NextFunction): void {
@@ -22,9 +23,8 @@ export class UsersController {
   }
 
   public getById(req: Request, res: Response, next: NextFunction): void {
-    if (!req.params.id)
-      return res.status(404).json({ message: 'Not Found' });
-    res.send('Hello i am users controller');
+    if (!req.params.id) return res.status(404).json({ message: "Not Found" });
+    res.send("Hello i am users controller");
   }
 }
 
